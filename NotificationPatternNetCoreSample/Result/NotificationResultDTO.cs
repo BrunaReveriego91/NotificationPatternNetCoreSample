430c4e604514874c33e3b490d17aa7402f05beec
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2022-11-03 17:08:33 -0300

    first commit

---
+++ NotificationPatternNetCoreSample/Result/NotificationResultDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace NotificationPatternNetCoreSample.Result
+{
+    public class NotificationResultDTO
+    {
+        public NotificationResultDTO(bool success, string message, object data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public bool Success { get; private set; }
+        public string Message { get; private set; }
+        public object Data { get; private set; }
+
+    }
+}
---
+++ NotificationPatternNetCoreSample/Verifier.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidator;
+using NotificationPatternNetCoreSample.Entity;
+using NotificationPatternNetCoreSample.Result;
+using System;
+
+namespace NotificationPatternNetCoreSample
+{
+    public class Verifier : Notifiable
+    {
+        public NotificationResultDTO Execute()
+        {
+            var person = new Person("Bruna Reveriego", "teste@gmail.com");
+            return Verify(person);
+        }
+
+        private NotificationResultDTO Verify(Person person)
+        {
+            //Simulating validation of entity
+            AddNotifications(person.Notifications);
+            if (Invalid)
+            {
+                return new NotificationResultDTO(false, "Please validate following fields:", Notifications);
+            }
+
+            return new NotificationResultDTO(true, "OK", null);
+        }
+    }
+}
